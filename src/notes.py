1aca85b20bb825f494373eff1745248f0bf0a81f
Nazar Chaban <134311145+Vivern0@users.noreply.github.com>
2024-01-12 23:53:29 +0200

    save/load feature
    clear_notes була для тесту, але можна її лишити

--- src/notes.py
+++ src/notes.py
@@ -1,22 +1,23 @@
 from datetime import datetime
+import csv
 
 
 class Note:
 
-    def __init__(self, *data):
+    def __init__(self, *data, date=None):
         self.author = data[0]
         self.title = data[1]
         self.note = data[2]
         self.tags = data[3:] if len(data) > 3 else None
-        self.date = datetime.now().strftime("%a %d %b %Y, %I:%M%p")
+        self.date = datetime.now().strftime("%a %d %b %Y, %I:%M%p") if date is None else date
 
     def change_note(self, field, new_data):
         fields_mapping = {
             "author": "author",
             "title": "title",
             "note": "note",
-            "date": "date",
-            "tags": "tags"
+            "tags": "tags",
+            "date": "date"
         }
 
         if field in fields_mapping:
@@ -25,8 +26,8 @@ class Note:
             print(f"Invalid field: {field}")
 
     def __str__(self):
-        tags_str = f", tags: {', '.join(self.tags)}" if self.tags else ""
-        return f"author: {self.author}, title: {self.title}, note: {self.note}, date: {self.date}{tags_str}"
+        tags_str = f"tags: {', '.join(self.tags)}, " if self.tags else ""
+        return f"author: {self.author}, title: {self.title}, note: {self.note}, {tags_str}date: {self.date}"
 
 
 class NoteManager:
@@ -44,6 +45,39 @@ class NoteManager:
             print("Note removed successfully.")
         else:
             print("Note not found in the list.")
+
+    def save_notes(self):
+        with open("notes_save.csv", "w", newline='') as fd:
+            fields = ["author", "title", "note", "tags", "date"]
+            writer = csv.DictWriter(fd, fieldnames=fields)
+            writer.writeheader()
+
+            for note in self.notes:
+                writer.writerow({
+                    "author": note.author,
+                    "title": note.title,
+                    "note": note.note,
+                    "tags": note.tags,
+                    "date": note.date
+                })
+
+    def load_notes(self):
+        with open("notes_save.csv", "r") as fd:
+            reader = csv.DictReader(fd)
+            for row in reader:
+                tegs_list = row["tags"].split(",") if row["tags"] else []
+                note = Note(
+                    row["author"],
+                    row["title"],
+                    row["note"],
+                    *tegs_list,
+                    date=row["date"]
+                )
+                self.notes.append(note)
+
+    def clear_notes(self):
+        self.notes = []
+        print("Notes cleared successfully.")
 
     def print_notes(self):
         if not self.notes:
