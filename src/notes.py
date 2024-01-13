213c600e7b108ba8ed452de741851ced64ee4d79
Nazar Chaban <134311145+Vivern0@users.noreply.github.com>
2024-01-13 18:22:25 +0200

    New features
    Додав add_tag, search_notes, search_notes_by_tag, змінив формат дати, щоб користувачу було легше міняти її та шукати по ній, валідатор при зміні дати

--- src/notes.py
+++ src/notes.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import csv
+import re
 
 
 class Note:
@@ -9,7 +10,7 @@ class Note:
         self.title = data[1]
         self.note = data[2]
         self.tags = ", ".join(data[3:]) if len(data) > 3 else None
-        self.date = datetime.now().strftime("%a %d %b %Y, %I:%M%p") if date is None else date
+        self.date = datetime.now().strftime("%d.%m.%Y, %H:%M") if date is None else date
 
     def change_note(self, field, new_data):
         fields_mapping = {
@@ -21,6 +22,11 @@ class Note:
         }
 
         if field in fields_mapping:
+            if field == 'date':
+                # Перевірка є тільки на формат дати
+                date_pattermn = re.compile(r"^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.\d{4}, (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$")
+                if not date_pattermn.math(new_data):
+                    raise ValueError("Invalid date format. Should be DD.MM.YYYY, HH:MM")
             setattr(self, fields_mapping[field], new_data)
         else:
             print(f"Invalid field: {field}")
@@ -45,6 +51,32 @@ class NoteManager:
             print("Note removed successfully.")
         else:
             print("Note not found in the list.")
+
+    def add_tag(self, note, tag):
+        if note in self.notes:
+            note.tags += ", " + tag
+            print("Tag added successfully.")
+        else:
+            print("Note not found in the list.")
+
+    def search_notes(self, info):
+        result = []
+        for note in self.notes:
+            if info in note.author or info in note.title or info in note.note or info in note.tags or info in note.date:
+                result.append(note)
+        return result
+
+    def search_notes_by_tags(self, search_tags:str):
+        # Користувач вводить через кому теги, ця функція їх розбиває на список
+        # і для кожного тегу провводить пошук по нотатках
+        search_tag_list = [x.strip() for x in search_tags.split(",")]
+        result = set()
+        for note in self.notes:
+            for tag in search_tag_list:
+                if tag in note.tags:
+                    result.add(note)
+        return result
+            
 
     def save_notes(self):
         with open("notes_save.csv", "w", newline='') as fd:
@@ -88,19 +120,66 @@ class NoteManager:
 
 # note_manager = NoteManager()
 
-# new_note = Note("John Doe", "Meeting", "Discuss project progress.", "important", "project")
-# note_manager.add_note(new_note)
+# new_note1 = Note("Person1", "Meeting", "Discuss project progress.", "important", "project")
+# note_manager.add_note(new_note1)
+
+# new_note2 = Note("Person2", "Work", "Don't forget about project.", "not so important", "project")
+# note_manager.add_note(new_note2)
+
+# new_note3 = Note("Person3", "Walk", "Don't go through the park.", "silly", "outside")
+# note_manager.add_note(new_note3)
+
+# new_note4 = Note("Person4", "Shopping", "Buy new coat.", "winter", "clothes")
+# note_manager.add_note(new_note4)
+
+# new_note5 = Note("Person5", "Hollyday", "Mayami has cool cafes.", "cafes", date="22.12.2010, 22:59")
+# note_manager.add_note(new_note5)
+
+# new_note6 = Note("Person6", "Sleep", "Go to sleep at 11PM.", "important", "sleep")
+# note_manager.add_note(new_note6)
+
+# #--------------------------------------------
 
 # print("\nInitial state:")
 # note_manager.print_notes()
 
-# print(new_note.author)
-# print(new_note.title)
-# print(new_note.note)
-# print(new_note.tags)
-# print(new_note.date)
+# #--------------------------------------------
 
-# note_manager.remove_note(new_note)
+# res = note_manager.search_notes("important")
+# print(f"\nSearch result (important):")
+# for i in res:
+#     print(f"\t- {str(i)}")
+
+# res = note_manager.search_notes("sleep")
+# print(f"\nSearch result (sleep):")
+# for i in res:
+#     print(f"\t- {str(i)}")
+
+# res = note_manager.search_notes("22.12.2010, 22:59")
+# print(f"\nSearch result (22.12.2010, 22:59):")
+# for i in res:
+#     print(f"\t- {str(i)}")
+
+# res = note_manager.search_notes("Don't")
+# print(f"\nSearch result (Don't):")
+# for i in res:
+#     print(f"\t- {str(i)}")
+
+# res = note_manager.search_notes("Person3")
+# print(f"\nSearch result (Person3):")
+# for i in res:
+#     print(f"\t- {str(i)}")
+
+# #--------------------------------------------
+
+# print("\nSearch by tags:")
+# res = note_manager.search_notes_by_tags("important, winter")
+# print(f"\nSearch result (important, winter):")
+# for i in res:
+#     print(f"\t- {str(i)}")
+
+# #--------------------------------------------
+# note_manager.clear_notes()
 
 # print("\nUpdated state:")
 # note_manager.print_notes()
